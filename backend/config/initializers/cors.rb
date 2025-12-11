bb1f8959ff0d1da6ed4045eff72b0190180ad60f
d42null <d2null@gmail.com>
2025-12-11 21:12:24 +0200

    Expose pagination headers in CORS

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -15,7 +15,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ["Authorization"],
+      expose: ["Authorization", "Total-Pages", "Current-Page"],
       credentials: true
   end
 end
