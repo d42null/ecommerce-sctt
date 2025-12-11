ff726d7efc28d6e49904247cce428594b0175357
d42null <d2null@gmail.com>
2025-12-11 20:44:49 +0200

    Implement Manual Pagination (No gems)

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -3,11 +3,20 @@ class ItemsController < ApplicationController
   before_action :check_admin, except: [:index, :show]
 
   def index
+    page = (params[:page] || 1).to_i
+    per_page = 20
+    
     if params[:q].present?
-      @items = Item.where("name ILIKE ?", "%#{params[:q]}%")
+      query = Item.where("name ILIKE ?", "%#{params[:q]}%")
     else
-      @items = Item.all
+      query = Item.all
     end
+
+    total_count = query.count
+    @items = query.offset((page - 1) * per_page).limit(per_page)
+    
+    response.headers['Total-Pages'] = (total_count / per_page.to_f).ceil.to_s
+    response.headers['Current-Page'] = page.to_s
     
     render json: @items
   end
