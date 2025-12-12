274f30a5b5f824881d7ff89b3d8968b0568bfa5c
d42null <d2null@gmail.com>
2025-12-12 23:43:53 +0200

    style: move toaster position to bottom-right

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Toaster position="top-right" />
+      <Toaster position="bottom-right" />
       <Navbar />
       <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <Routes>
