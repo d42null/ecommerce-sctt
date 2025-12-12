6f87b7bc028ec44c41916d9b0a7cd724deb327cd
d42null <d2null@gmail.com>
2025-12-12 21:46:25 +0200

    fix(backend): force https full_host for omniauth in production

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,2 +1,6 @@
 OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.silence_get_warning = true
+
+if Rails.env.production?
+  OmniAuth.config.full_host = "https://#{ENV['HOST'] || 'ecommerce-sctt.onrender.com'}"
+end
